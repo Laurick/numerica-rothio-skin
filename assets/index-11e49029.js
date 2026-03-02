71ad4d20100ceeed248c54882515695d9a4dedda
Laurick <Laurick@users.noreply.github.com>
2026-03-02 08:39:18 +0000

    deploy: 57b19437e9f43d12dd786c3a66b7d2743e60186c

--- assets/index-11e49029.js
+++
@@ -1,1 +0,0 @@
-import{e as r,f as n,r as o,j as t}from"./index-23c0ca87.js";import{u}from"./index-07e43956.js";function c(){const{channel:e}=r(),s=n(),{setChannel:a}=u();return o.useEffect(()=>{e&&(a(e),s("/game"))},[e,a]),t.jsx(t.Fragment,{})}export{c as default};
